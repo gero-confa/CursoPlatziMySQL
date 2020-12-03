e6656aedd4211e23bab1cf8ad5dc5b166d4bcbe1
Gero <geronimo.confalonieri@gmail.com>
2020-12-03 15:20:39 -0300

    already inserting into that bad boi

--- books.sql
+++ books.sql
@@ -13,6 +13,31 @@ CREATE TABLE IF NOT EXISTS books (
 -- en la base esta como author pero aca lo dejo asi porque es buena practica a los objectos ponerlos en plural
 CREATE TABLE IF NOT EXISTS authors (
     author_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
     nationality VARCHAR(3)
 );
+CREATE TABLE IF NOT EXISTS clients(
+    client_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(40) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    birthdate DATETIME,
+    gender ENUM('M', 'F', 'ND'),
+    active TINYINT(1) NOT NULL DEFAULT 1,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+);
+CREATE TABLE IF NOT EXISTS operations(
+    operation_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    book_id INTEGER UNSIGNED NOT NULL,
+    client_id INTEGER UNSIGNED NOT NULL, 
+    -- S para sold y R para rented y RN para Redturned
+    operation_type ENUM('S', 'R', 'RN'),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    finished TINYINT(1) NOT NULL DEFAULT 1
+);
+-- INSERT INTO tabla(COLUMNAS*) VALUES(VALORES)
+INSERT INTO authors(author_id, name, nationality) VALUES('','Juan Rulfo','MEX');
+INSERT INTO authors(name, nationality) VALUES('Gabriel Garcia Márquez', 'COL');
+INSERT INTO authors() VALUES ('', 'Juan Gabriel Vazquez', 'COL');
